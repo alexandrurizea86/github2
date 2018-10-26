ef35cb30e7360aec42a8a5c0872419f35e8754d1
alexandrurizea86 <alexandrurizea86@users.noreply.github.com>
2018-10-26 16:16:52 +0300

    adaugare test1

---
+++ src/app/test1.java
@@ -0,0 +1,15 @@
+package app;
+
+public class test1 {
+	
+	public static void main(String[] args) {
+		
+		
+		for(int i=1; i<=10; i++) {
+			System.out.println("Numarul este " + i);
+		}
+		
+		
+	}
+
+}
